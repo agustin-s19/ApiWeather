76cc19ff635b33803c8e6e0c0954c14d9eddbf05
Agus <agustin_sabia@hotmail.com>
2024-02-28 18:32:53 -0300

    Agregar archivos de proyecto.

---
+++ ApiWeather/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ApiWeather.Models;
+using Newtonsoft.Json;
+
+public class HomeController : Controller
+{
+    private readonly string WeatherApiBaseUrl;
+    private readonly string WeatherApiKey;
+
+    public HomeController()
+    {
+        WeatherApiBaseUrl = ConfigurationManager.AppSettings["WeatherApiBaseUrl"];
+        WeatherApiKey = ConfigurationManager.AppSettings["WeatherApiKey"];
+    }
+
+    public ActionResult Index() {
+
+        return View(new PronosticoCiudad());
+        
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult> ObtenerTemperatura(PronosticoCiudad model)
+    {
+        if (string.IsNullOrEmpty(WeatherApiBaseUrl) || string.IsNullOrEmpty(WeatherApiKey))
+        {
+            ViewBag.Error = "Las configuraciones de la API no están configuradas correctamente.";
+            return View("Index", model);
+        }
+
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                var apiPronostico = $"{WeatherApiBaseUrl}forecast.json?key={WeatherApiKey}&q={model.nombreCiudad}&days={6}";
+                var response = await httpClient.GetAsync(apiPronostico);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                   
+                    var pronostico = JsonConvert.DeserializeObject(data);
+
+                    ViewBag.Temperatura = pronostico;
+
+                   
+                }
+                else
+                {
+                    ViewBag.Error = "Hubo un error al obtener los datos de temperatura.";
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                ViewBag.Error = "Hubo un error al procesar la solicitud: " + ex.Message;
+            }
+        }
+
+        return View("Index", model);
+    }
+}
+
---
+++ ApiWeather/Models/Ciudad.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiWeather.Models
+{
+    public class PronosticoDia
+    {
+        public string nombreCiudad {  get; set; }
+
+        public string Dia {  get; set; }
+        public int TemperaturaMin {  get; set; }
+
+        public int TemperaturaMax { get; set; }
+        
+       
+    }
+    public class PronosticoCiudad
+    {
+        public string nombreCiudad { get; set; }
+
+        public int TemperaturaActual { get; set; }
+
+        public int TemperaturaMinima {  get; set; }
+
+        public int TemperaturaMaxima { get; set; }
+
+        public List<PronosticoDia> PronosticoProximosDias { get; set; }
+
+      
+    }
+
+ 
+
+
+
+
+}
